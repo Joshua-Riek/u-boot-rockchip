7cb6072b1d9be7ee8de45f0516895402f30a10c4
Joshua Riek <jjriek@verizon.net>
2024-09-08 12:15:32 -0400

    Import debian packaging

---
+++ debian/targets.mk
@@ -0,0 +1,37 @@
+# Target platforms supported by u-boot.
+# debian/rules includes this Makefile snippet.
+
+u-boot-rockchip_platforms += turing-rk1-rk3588
+turing-rk1-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_uart9_115200_v1.16.bin
+turing-rk1-rk3588_bl31 := rk3588_bl31_v1.45.elf
+turing-rk1-rk3588_pkg := turing-rk1
+
+u-boot-rockchip_platforms += orangepi-5-rk3588s
+orangepi-5-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+orangepi-5-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+orangepi-5-rk3588s_pkg := orangepi-5
+
+u-boot-rockchip_platforms += orangepi-5b-rk3588s
+orangepi-5b-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+orangepi-5b-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+orangepi-5b-rk3588s_pkg := orangepi-5b
+
+u-boot-rockchip_platforms += orangepi-5-plus-rk3588
+orangepi-5-plus-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+orangepi-5-plus-rk3588_bl31 := rk3588_bl31_v1.45.elf
+orangepi-5-plus-rk3588_pkg := orangepi-5-plus
+
+u-boot-rockchip_platforms += rock5a-rk3588s
+rock5a-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+rock5a-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+rock5a-rk3588s_pkg := rock-5a
+
+u-boot-rockchip_platforms += rock5b-rk3588
+rock5b-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+rock5b-rk3588_bl31 := rk3588_bl31_v1.45.elf
+rock5b-rk3588_pkg := rock-5b
+
+u-boot-rockchip_platforms += nanopc-t6-rk3588
+nanopc-t6-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+nanopc-t6-rk3588_bl31 := rk3588_bl31_v1.45.elf
+nanopc-t6-rk3588_pkg := nanopc-t6
