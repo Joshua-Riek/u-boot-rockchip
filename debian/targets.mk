326b22b9b190d29885916206137c556db3f2efb8
Joshua Riek <jjriek@verizon.net>
2024-12-21 12:14:56 -0500

    Import debian packaging

---
+++ debian/targets.mk
@@ -0,0 +1,52 @@
+# Target platforms supported by u-boot.
+# debian/rules includes this Makefile snippet.
+
+u-boot-rockchip_platforms += turing-rk1-rk3588
+turing-rk1-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_uart9_115200_v1.16.bin
+turing-rk1-rk3588_bl31 := rk3588_bl31_v1.45.elf
+turing-rk1-rk3588_pkg := turing-rk1
+
+u-boot-rockchip_platforms += orangepi-5-rk3588s
+orangepi-5-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+orangepi-5-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+orangepi-5-rk3588s_pkg := orangepi-5
+
+u-boot-rockchip_platforms += orangepi-5b-rk3588s
+orangepi-5b-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+orangepi-5b-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+orangepi-5b-rk3588s_pkg := orangepi-5b
+
+u-boot-rockchip_platforms += orangepi-5-plus-rk3588
+orangepi-5-plus-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+orangepi-5-plus-rk3588_bl31 := rk3588_bl31_v1.45.elf
+orangepi-5-plus-rk3588_pkg := orangepi-5-plus
+
+u-boot-rockchip_platforms += rock5a-rk3588s
+rock5a-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+rock5a-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+rock5a-rk3588s_pkg := rock-5a
+
+u-boot-rockchip_platforms += rock5b-rk3588
+rock5b-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+rock5b-rk3588_bl31 := rk3588_bl31_v1.45.elf
+rock5b-rk3588_pkg := rock-5b
+
+u-boot-rockchip_platforms += rock-5-itx-rk3588
+rock-5-itx-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+rock-5-itx-rk3588_bl31 := rk3588_bl31_v1.45.elf
+rock-5-itx-rk3588_pkg := rock-5-itx
+
+u-boot-rockchip_platforms += nanopi-r6c-rk3588s
+nanopi-r6c-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+nanopi-r6c-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+nanopi-r6c-rk3588s_pkg := nanopi-r6c
+
+u-boot-rockchip_platforms += nanopi-r6s-rk3588s
+nanopi-r6s-rk3588s_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+nanopi-r6s-rk3588s_bl31 := rk3588_bl31_v1.45.elf
+nanopi-r6s-rk3588s_pkg := nanopi-r6s
+
+u-boot-rockchip_platforms += nanopc-t6-rk3588
+nanopc-t6-rk3588_ddr := rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin
+nanopc-t6-rk3588_bl31 := rk3588_bl31_v1.45.elf
+nanopc-t6-rk3588_pkg := nanopc-t6
