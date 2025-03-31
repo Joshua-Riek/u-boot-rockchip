262fa88721d7ea9a19a4a61083cbea1790a16424
Joshua-Riek <jjriek@verizon.net>
2025-03-31 01:32:35 -0400

    f

--- drivers/power/fuel_gauge/fg_sbs-battery.c
+++ drivers/power/fuel_gauge/fg_sbs-battery.c
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 // Author: Yang Tsao<yang@fydeos.io>
-#define DEBUG 1
 #include <common.h>
 #include <dm.h>
 #include <errno.h>
